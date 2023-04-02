7dff5eb4e59c3e742c8dd3157eb340b40c0033b7
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2023-04-02 21:57:40 +0800

    course 0402

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player2 : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public Rigidbody rb;
     public float force;
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(new Vector3(force , 0 , 0) * Time.deltaTime);
             //transform.position += new Vector3(0, 0, 0.05f);
             //transform.position = transform.position + new Vector3(0, 0, 0.01f);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(new Vector3(-force, 0, 0) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             rb.AddForce(new Vector3(0, 0, force) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(new Vector3(0, 0, -force) * Time.deltaTime);
         }
