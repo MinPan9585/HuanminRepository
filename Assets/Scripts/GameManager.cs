7b202de464e1197ca1327d3ca5bb43f9a446a054
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2023-04-16 22:00:20 +0800

    COURSE 0416

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     //public bool isPaused = false;
+    public AudioSource bgm;
 
     public void SetGamePause()
     {
         Time.timeScale = 0;
+        bgm.Pause();
         //isPaused = true;
     }
 
     public void SetGameContinue()
     {
         Time.timeScale = 1;
+        bgm.Play();
     }
 }
