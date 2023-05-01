1c87204b914e08e2e48f51bb1a317dcd2011460a
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2023-05-01 21:57:15 +0800

    course 0501

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,21 +8,21 @@ public class Player2 : MonoBehaviour
     public float force;
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddForce(new Vector3(force , 0 , 0) * Time.deltaTime);
             //transform.position += new Vector3(0, 0, 0.05f);
             //transform.position = transform.position + new Vector3(0, 0, 0.01f);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.AddForce(new Vector3(-force, 0, 0) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(new Vector3(0, 0, force) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             rb.AddForce(new Vector3(0, 0, -force) * Time.deltaTime);
         }
--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -8,21 +8,21 @@ public class Player : MonoBehaviour
     public float force;
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(new Vector3(force , 0 , 0) * Time.deltaTime);
             //transform.position += new Vector3(0, 0, 0.05f);
             //transform.position = transform.position + new Vector3(0, 0, 0.01f);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.A))
         {
             rb.AddForce(new Vector3(-force, 0, 0) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(new Vector3(0, 0, force) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(new Vector3(0, 0, -force) * Time.deltaTime);
         }
